73e33f38766a62c8d3648a22eb2f43e035c8004e
JosephSKazzi <48451002+JosephSKazzi@users.noreply.github.com>
2020-07-06 21:17:01 +0300

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\asklebanonbot-Web-Deploy.pubxml -p:Password=qagNhGq5ozkYtk7X8PPnynXLuotledgxuP5R6R1k4JgcXcxGDoaSHqGdQhWu
+
